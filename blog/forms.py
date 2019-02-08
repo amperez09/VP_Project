44cec5c93c2e78cef5eabbb7440c9bb5d0f43987
Jose-Oton <joseotoncv@gmail.com>
2019-02-08 12:14:37 -0600

    Fixed templates

---
+++ blog/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.core.validators import validate_email
+
+#Employee
+class EmployeeForm(forms.Form):
+	firstName = forms.CharField(label='Nombre', max_length=100, 
+		widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Juanito','type': 'text'}))
+
+	lastName_1 = forms.CharField(label='Apellido Paterno', max_length=100,
+		widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Pérez','type': 'text'}))
+
+	lastName_2 = forms.CharField(label='Apellido Materno', max_length=100,
+		widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Camacho','type': 'text'}))
+	
+	country = forms.ChoiceField(choices=[('MX','MX'),('ES','ES')])
+		#widget=forms.TextInput(attrs={'class':'dropdown-menu'}))
+
+	email = forms.EmailField(label='Email',
+		widget=forms.TextInput(attrs={'class':'form-control','placeholder':'juanito@datiobd.com','type':'email'}))
+
+	role = forms.CharField(label='Área', max_length=100,
+		widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Deployment'}))
+
+	percentage = forms.FloatField(label='Porcentaje',widget=forms.TextInput(attrs={'class': 'form-control','placeholder':'0.4'}))
+
+	project = forms.ChoiceField(choices=[('Thor USA','Thor USA'),
+		('Guardianes MX','Guardianes MX'),('Soporte MX','Soporte MX')],required=True)
+		#widget=forms.TextInput(attrs={'class': 'dropdown'}))
+
+#ProjectForm
+class ProjectForm(forms.Form):
+	codeProject = forms.CharField(label='Código',max_length=50,
+		widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': '3BDH.BBV0100'}))
+
+	nameProject = forms.CharField(label='Nombre del proyecto',max_length=100,
+		widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'DAT:CDR DevOps MX'}))
+	
+	typeProject = forms.CharField(label='Tipo',max_length=10,
+		widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'IN'}))
+
+	ftes = forms.CharField(label='FTES',max_length=50,
+		widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Deployment'}))
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,99 @@
 from django.shortcuts import render
-
+from .forms import EmployeeForm,ProjectForm
 # Create your views here.
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+#Add employee
 def add_empleado(request):
-    return render(request, 'blog/add_empleado.html', {})
+	# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EmployeeForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+            print(request)
+            return render(request, 'blog/add_empleado.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'blog/add_empleado.html', {'form': form})
+
+#Add project
+def add_proyecto(request):
+	# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ProjectForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+            print(request.POST)
+            return render(request, 'blog/add_proyecto.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ProjectForm()
+
+    return render(request, 'blog/add_proyecto.html', {'form': form})
+
 def empleado(request):
     return render(request,'blog/empleado.html',{})
+
 def proyecto(request):
     return render(request,'blog/proyecto.html',{})
-def add_proyecto(request):
-    return render(request,'blog/add_proyecto.html',{})
+
 def mod_proyecto(request):
-    return render(request,'blog/mod_proyecto.html',{})
+	# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ProjectForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+            print(request.POST)
+            return render(request, 'blog/mod_proyecto.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ProjectForm()
+
+    return render(request, 'blog/mod_proyecto.html', {'form': form})
+
+
+
 def mod_empleado(request):
-    return render(request,'blog/mod_empleado.html',{})
+	# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EmployeeForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+            print(request)
+            return render(request, 'blog/mod_empleado.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'blog/mod_empleado.html', {'form': form})
+
 def reporte_general(request):
     return render(request,'blog/reporte_general.html',{})
